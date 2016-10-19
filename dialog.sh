55f25516916ef717df199bf991e0a1d821cf82d1
Reddriver <prochazka.lukas@centrum.cz>
2016-10-19 14:39:26 +0200

    Add option reboot

--- dialog.sh
+++ dialog.sh
@@ -14,8 +14,7 @@ zadaniHeslaRoot() {
 getStatusSudo() {
 	#test sudo opravneni
 	CAN_I_RUN_SUDO=$(sudo -n uptime 2>&1|grep "load"|wc -l)
-	if [ ${CAN_I_RUN_SUDO} = 0 ]
-	then
+	if [ ${CAN_I_RUN_SUDO} = 0 ]; then
 		zadaniHeslaRoot()
 	fi
 }
