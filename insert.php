8def604b5b0b69ce14a474cba296073187caf218
reddriver <lukasprochajda@gmail.com>
2016-12-04 13:29:20 +0100

    add obyvak mereni

--- insert.php
+++ insert.php
@@ -49,22 +49,20 @@ if (isset($_GET["serial"]) && isset($_GET["id_veliciny"]) && isset($_GET["hodnot
 
 	if ($result->num_rows > 0) {
 	    while($row = $result->fetch_assoc()) {
-	        //echo "<br> Predan zaznam pro domaciMeteo: %" . number_format($row["hodnota"],2) . "*";
 	        $predani = $predani . number_format($row["hodnota"],2);
 	    }
 	} else {
     	echo "0 results";
 	}
-
+	$predani = $predani . ";";
 	if ($result2->num_rows > 0) {
 		while($row2 = $result2->fetch_assoc()) {
 			 $predani = $predani . number_format($row2["hodnota"],2);
-			//echo "<br> Predan zaznam2 pro domaciMeteo: %" . number_format($row["hodnota"],2) . "*";
 		}
 	} else {
 		echo "0 results";
 	}
-	echo "<br> Predan zaznam2 pro domaciMeteo: %" . predani . "*";
+	echo "<br> Predan zaznam pro domaciMeteo: %" . $predani . "*";
 
 	$conn->close();
 }else{
