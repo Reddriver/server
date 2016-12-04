41eeec1c5975840511f7f92998f96182b22b3a67
reddriver <lukasprochajda@gmail.com>
2016-12-04 11:40:19 +0100

    rename VenkovniMeteoNodeMCU to MeteoNodeMCU

---
+++ MeteoNodeMCU/MeteoNodeMCU.ino
@@ -0,0 +1,86 @@
+/*
+ *  This sketch sends data via HTTP GET requests to data.sparkfun.com service.
+ *
+ *  You need to get streamId and privateKey at data.sparkfun.com and paste them
+ *  below. Or just customize this script to talk to other HTTP servers.
+ *
+ */
+
+#include <ESP8266WiFi.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+#define ONE_WIRE_BUS D1
+
+const char* ssid     = "Prochazkovi";
+const char* password = "aximGAM123";
+const char* host = "farma.trianglstudio.cz";
+
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature DS18B20(&oneWire);
+char temperatureString[6];
+
+float getTemperature() {
+  float temp;
+  do {
+    DS18B20.requestTemperatures(); 
+    temp = DS18B20.getTempCByIndex(0);
+    delay(100);
+  } while (temp == 85.0 || temp == (-127.0));
+  return temp;
+}
+
+void setup() {
+  Serial.begin(115200);
+  delay(10);
+  WiFi.begin(ssid, password);
+  DS18B20.begin();
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+  }
+}
+
+int value = 0;
+
+void loop() { 
+  ++value;
+  float temperature = getTemperature();
+  dtostrf(temperature, 2, 2, temperatureString);
+  
+  // Use WiFiClient class to create TCP connections
+  WiFiClient client;
+  const int httpPort = 80;
+  if (!client.connect(host, httpPort)) {
+    Serial.println("connection failed");
+    return;
+  }
+  
+  String url = "/insert.php?serial=teplotaVenku&&id_veliciny=5&&hodnota=";
+  url += temperatureString;
+  Serial.println(url);
+    
+  // This will send the request to the server
+  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" + 
+               "Connection: close\r\n\r\n");
+  unsigned long timeout = millis();
+  while (client.available() == 0) {
+    if (millis() - timeout > 5000) {
+      Serial.println(">>> Client Timeout !");
+      client.stop();
+      return;
+    }
+  }
+  
+  // Read all the lines of the reply from server and print them to Serial
+  while(client.available()){
+    String line = client.readStringUntil('\r');
+    Serial.print(line);
+  }
+  
+  Serial.println();
+  Serial.println("closing connection");
+  delay(300000);
+}
+
