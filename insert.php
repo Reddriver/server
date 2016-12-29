eecc1a2d5d9336de72d4b0d0912c2d6f62181a48
Reddriver <prochazka.lukas@centrum.cz>
2016-12-29 19:23:24 +0100

    Update insert.php
    Pridani predani obyvaku pro meteopokoj

--- insert.php
+++ insert.php
@@ -6,73 +6,41 @@ if (isset($_GET["serial"]) && isset($_GET["id_veliciny"]) && isset($_GET["hodnot
 	$id_veliciny = $_GET["id_veliciny"];
 	$hodnota = $_GET["hodnota"];
 
-	//mysql
-	$username = "luka";
-	$password = "gamepark";
-	$hostname = "localhost"; 
-	$dbname = "luka";
-
-  if($hodnota < 45 && $hodnota > -30){
-    if($id_veliciny == 1) {	
-	$retVal = rrd_update("/var/www/html/teplotyVenku.rrd", (array)"N:$hodnota");	    
-	echo "Ulozeno do RRD = " . $retVal;	
-	$xml = file_get_contents("http://reddriver.tmep.cz/?temp=" . $hodnota);
-	$xmlText = "https://script.google.com/macros/s/AKfycbx0HXK9ciYqRig6dqfV2_JSCoDO9g4OBdM8ye1q_764Xxra7Us/exec?teplota=" . $hodnota;
-	echo $xmlText;
-	$xml2 = file_get_contents($xmlText);
-    }
-    if($id_veliciny == 3) {
-	$retVal = rrd_update("/var/www/html/teploty.rrd", (array)"N:$hodnota");
-	$xml = file_get_contents("http://reddriver.tmep.cz/?temp2=" . $hodnota);
-      echo "Ulozeno do RRD = " . $retVal;
-    }    
+	if($hodnota < 45 && $hodnota > -30){
+	    if($id_veliciny == 1) {	
+			$xml = file_get_contents("http://reddriver.tmep.cz/?temp=" . $hodnota);
+			$xmlText = "https://script.google.com/macros/s/AKfycbx0HXK9ciYqRig6dqfV2_JSCoDO9g4OBdM8ye1q_764Xxra7Us/exec?teplota=" . $hodnota;
+			echo $xmlText;
+			$xml2 = file_get_contents($xmlText);
+	    }
+	    if($id_veliciny == 3) {
+			$xml = file_get_contents("http://reddriver.tmep.cz/?temp2=" . $hodnota);
+	    }    
 	}	
 
   if($hodnota < 95 && $hodnota > 30){
   	if($id_veliciny == 4) {
-		$retVal = rrd_update("/var/www/html/vlhkosti.rrd", (array)"N:$hodnota");
-      		echo "Ulozeno do RRD = " . $retVal;
 		$xml = file_get_contents("http://reddriver.tmep.cz/?humV=" . $hodnota);
-	 }
-  }  
-  
-  echo "<br>";
-  
-	$conn =  mysqli_connect($hostname, $username, $password, $dbname);
-	$sql = "INSERT INTO zaznamy (cidlo, id_veliciny, hodnota) VALUES ('" . $cidlo . "', " . $id_veliciny . ", " . $hodnota . ")";
-	$sql2 = "select hodnota from zaznamy where id_veliciny = 1 order by cas DESC LIMIT 1";
-	$sql3 = "select hodnota from zaznamy where id_veliciny = 5 order by cas DESC LIMIT 1";
-	
-	if ($conn->query($sql) === TRUE) {
-		echo " Zaznam do mysql ulozen!";
-	} else {
-		echo "Error: " . $sql . "<br>" . $conn->error;
 	}
-	
-	
-	$result = $conn->query($sql2);
-	$result2 = $conn->query($sql3);
-	$predani = "";
-
-	if ($result->num_rows > 0) {
-	    while($row = $result->fetch_assoc()) {
-	        $predani = $predani . number_format($row["hodnota"],2);
-	    }
-	} else {
-    	echo "0 results";
-	}
-	$predani = $predani . ";";
-	if ($result2->num_rows > 0) {
-		while($row2 = $result2->fetch_assoc()) {
-			 $predani = $predani . number_format($row2["hodnota"],2);
-		}
-	} else {
-		echo "0 results";
-	}
-	echo "<br> Predan zaznam pro domaciMeteo: %" . $predani . "*";
-
-	$conn->close();
+  }   
 }else{
-	echo "Neni predan nektery z parametru metodou GET!";
+	echo "Neni predan nektery z parametru metodou GET!<br>";
+}
+
+$urlTeplotaVenku = "https://tmep.cz/vystup-json.php?id=423&export_key=byrl2kn1vq"; 
+$urlTeplotaObyvak = "https://tmep.cz/vystup-json.php?id=467&export_key=r9uqxo5znf";
+$json1 = file_get_contents($urlTeplotaVenku);
+$json2 = file_get_contents($urlTeplotaObyvak);
+$teplotaVenku = json_decode($json1);
+$teplotaObyvak = json_decode($json2);
+
+echo "Předávám: ";
+
+if(is_numeric($teplotaVenku->teplota)){
+  echo "%" . round($teplotaVenku->teplota, 1) . "*";
+}
+
+if(is_numeric($teplotaObyvak->teplota)){
+  echo "@" . round($teplotaObyvak->teplota, 1) . "§";
 }
 ?>
